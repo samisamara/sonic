da0be9997beb86b339e9f016d356afee1d1979c8
sam <someone@someplace.com>
2022-08-05 03:21:22 -0500

    commented out console logs to simplify future testing

--- javascript/tester.js
+++ javascript/tester.js
@@ -965,8 +965,8 @@ const numArray = [1, 2, 3];
 const doubleNumArray = numbers.map(num => {
     return num * 2;
 });
-console.log(numArray);
-console.log(doubleNumArray);
+// console.log(numArray);
+// console.log(doubleNumArray);
 
 
 
